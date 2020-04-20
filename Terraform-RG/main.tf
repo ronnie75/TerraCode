bbda3e263f92a0541ba82b9f32d91ba067faf7ff
Marco Verrati <mverrati@psg.com>
2020-04-20 09:16:26 -0400

    test

---
+++ Terraform-RG/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tstate"
+    storage_account_name  = "tstate14359"
+    container_name        = "tstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+    }
+    
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-demo"
+  location = "eastus"
+}
