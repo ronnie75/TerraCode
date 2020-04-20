837bf7b2b275fc4b72cf38795097c973469a6782
Marco Verrati <mverrati@psg.com>
2020-04-20 09:49:21 -0400

    Revert "test de modification"
    This reverts commit 6b1bd9c5549550b90cb6098100518a69bfd86ba2.
    
    :q

--- Terraform-RG/main.tf
+++ Terraform-RG/main.tf
@@ -10,7 +10,6 @@ terraform {
 provider "azurerm" {
     # The "feature" block is required for AzureRM provider 2.x. 
     # If you're using version 1.x, the "features" block is not allowed.
-    # test de modification
     version = "~>2.0"
     features {}
     }
